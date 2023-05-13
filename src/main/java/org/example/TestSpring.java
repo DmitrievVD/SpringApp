41b112d0d2daa1c7707fecfe1d5821577957e86e
Viktor <fokus11548@mail.ru>
2023-05-13 16:04:32 +0500

    Test Bean

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,5 +6,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+
     }
 }
